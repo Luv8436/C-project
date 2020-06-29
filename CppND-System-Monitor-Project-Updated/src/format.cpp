94cc1a9ef48d079ecaa3897c80ce0ad5f5dc2fd2
Luvkumar8436 <luvkumar8436@gmail.com>
2020-06-29 13:30:41 +0530

    change format

--- CppND-System-Monitor-Project-Updated/src/format.cpp
+++ CppND-System-Monitor-Project-Updated/src/format.cpp
@@ -8,4 +8,13 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    std::string time;
+    int hour = seconds / (60*60) ;
+    double remSeconds = seconds - hour*60*60;
+    int  min = remSeconds / 60 ;
+    remSeconds = remSeconds - min*60;
+    int second = remSeconds;
+    time = std::to_string(hour)+":"+std::to_string(min)+":"+std::to_string(second);
+    return time;
+}
